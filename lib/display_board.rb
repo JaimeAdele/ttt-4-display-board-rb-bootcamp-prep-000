95deb3f7f9925ee995639166c87a87e756051903
Jaime Zuspann <jaimeadele@gmail.com>
2018-01-04 18:22:35 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,14 +2,14 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def display_board
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def x_turn
+def x_turn(board)
   print "Enter a number from 1 to 9: "
   move = gets.chomp.to_i - 1
-  display_board
+  board[move] = "X"
 end
 
 def o_turn
